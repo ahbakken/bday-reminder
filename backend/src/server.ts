36dc337ffe7425ab153d379c2e680565fa939130
Astrid Bakken <astridbakken95@gmail.com>
2023-03-05 21:25:01 +0100

    feat: backend: init project fastify

---
+++ backend/src/server.ts
@@ -0,0 +1,22 @@
+// Backend API RESTful
+
+import Fastify from "fastify";
+
+
+const app = Fastify();
+
+/**
+ * Metodo HTTP: Get, Post, Put, Patch, Delete
+ */
+
+app.get("/", async () => {
+  return { hello: "world" };
+});
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("Server running!");
+  });
