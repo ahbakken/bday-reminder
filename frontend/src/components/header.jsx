71ec6a8f180569041e060663864d38c364e47d77
Astrid Bakken <astridbakken95@gmail.com>
2023-03-06 00:10:16 +0100

    feat: frontend: header: icons, skeleton, bg-col

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React from "react";
+import { AiOutlineHome } from "react-icons/ai";
+import { AiOutlineCalendar } from "react-icons/ai";
+import Link from "next/link";
 
 // Tips: logo home, summary : logout, home, list
 
 const header = () => {
   return (
-    <div>header here !!!</div>
-  )
-}
+    <div className="flex h-20 w-full flex-row justify-between items-center bg-gradient-to-r from-[var(--color-secondary)] to-[var(--color-primary)] px-7">
+      <div>
+        <div className="">
+          <h1 className="font-bold text-2xl hover:scale-105 trasnform">Bday Reminder</h1>
+        </div>
+      </div>
+      <div className="flex flex-row justify-around">
+        <div className="flex flex-row justify-around text-white hover:scale-105 trasnform">
+          <div>
+            <Link href="/">
+              <p className="text-xl">Calendar</p>
+            </Link>
+          </div>
+          <div className="px-2">
+            <Link href="/">
+              <AiOutlineCalendar size={25} />
+            </Link>
+          </div>
+        </div>
+        <div className="flex flex-row justify-around text-white hover:scale-105 trasnform">
+          <div>
+            <Link href="/">
+              <p className="text-xl">Home</p>
+            </Link>
+          </div>
+          <div className="px-2">
+            <Link href="/">
+              <AiOutlineHome size={25} />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default header
+export default header;
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,20 +1,20 @@
-import {useState, useEffect} from "react"
-import Footer from "../components/footer"
-import Header from "../components/header"
-
+import { useState, useEffect } from "react";
+import Footer from "../components/footer";
+import Header from "../components/header";
 
 //Todo dont forget about commits!!! do it!
 
 export default function Home() {
+  //Gostosa, love you more dengo!!!!!!
 
   return (
     <>
-    <Header/>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <Footer/>
-    </>
+      <Header />
+      <div>
+        <h1 className="text-2xl">This is the page</h1>
+      </div>
 
-  )
+      <Footer />
+    </>
+  );
 }
